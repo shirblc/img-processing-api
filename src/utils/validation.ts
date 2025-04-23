6300ee0ba65566b5f78e9a3b1d3ead7b80da8f36
Shir Bar Lev <shirblc@gmail.com>
2025-04-23 13:29:14 +0300

    Added a validation middleware for the GET /images request.

---
+++ src/utils/validation.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from "express";
+
+const defaultHeight = 200;
+const defaultWidth = 200;
+
+/**
+ * Validates the inputs to the GET /images endpoint and converts
+ * the numeric inputs to numbers.
+ * @param req the incoming request
+ * @param res the outgoing response
+ * @param next the next middleware in the chain
+ */
+export function validateImageInputs(req: Request, res: Response, next: NextFunction): void {
+  const requestedImageName = req.query.image as string | undefined;
+  if (!requestedImageName) {
+    console.error("Invalid request: Image name is required");
+    res.status(400).send("Image name is required");
+  }
+
+  let requestedHeight = Number(req.query.height as string | undefined);
+  let requestedWidth = Number(req.query.width as string | undefined);
+
+  if (!requestedHeight) {
+    console.warn(
+      `No requested height for image ${requestedImageName}. Setting the default ${defaultHeight}.`,
+    );
+    requestedHeight = defaultHeight;
+  }
+
+  if (!requestedWidth) {
+    console.warn(
+      `No requested width for image ${requestedImageName}. Setting the default ${defaultWidth}.`,
+    );
+    requestedWidth = defaultWidth;
+  }
+
+  req.image = {
+    name: requestedImageName as string,
+    height: requestedHeight,
+    width: requestedWidth,
+  };
+
+  next();
+}
