10d33d89a4d9e834318719157958c2278bb767c8
Shir Bar Lev <shirblc@gmail.com>
2025-04-24 21:07:16 +0300

    The setup script now clears the tests' assets folder before running tests.
    This ensures that tests are always running in isolation and previous runs' files don't interfere with future runs.

--- test/helpers/setup.ts
+++ test/helpers/setup.ts
@@ -3,6 +3,8 @@ import { resolve } from "node:path";
 
 /** Copy the initial images so we can use them in tests */
 const rootPath = resolve(".");
-fsPromise
-  .cp(`${rootPath}/src/assets`, `${rootPath}/spec/src/assets`, { recursive: true })
-  .catch((error) => console.log(error));
+fsPromise.rm(`${rootPath}/spec/src/assets`, { recursive: true, force: true }).then(() => {
+  fsPromise
+    .cp(`${rootPath}/src/assets`, `${rootPath}/spec/src/assets`, { recursive: true })
+    .catch((error) => console.log(error));
+});
